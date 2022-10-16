7a2f53a8534cf4329aadc42f708650c1e910add3
Agunivers7 <herokuone7@gmail.com>
2022-10-16 12:53:56 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,36 +1,13 @@
-class Config(object):
-    LOGGER = True
-
-    # REQUIRED
-    API_KEY = "5546235142:AAGUa1r-5_N1joxXLWDvcjf8uUJjs2SgShA"
-    OWNER_ID = "1323557247"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "Spoidermon12"
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'postgres://apex:aadhi@8.219.177.252:5432/apexdb'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    LOAD = []
-    NO_LOAD = ['translation', 'rss']
-    WEBHOOK = False
-    URL = None
-
-    # OPTIONAL
-    SUDO_USERS = [1323557247]  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = [1323557247]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = [1323557247]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
-    BMERNU_SCUT_SRELFTI = 0
-
-class Production(Config):
-    LOGGER = False
+from tg_bot.sample_config import Config
 
 
 class Development(Config):
-    LOGGER = True
+    OWNER_ID = 1323557247  # my telegram ID
+    OWNER_USERNAME = "@Spoidermon12"  # my telegram username
+    API_KEY = "5546235142:AAGUa1r-5_N1joxXLWDvcjf8uUJjs2SgShA"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgres://apex:aadhi@8.219.177.252:5432/apexdb'  # sample db credentials
+    MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
+    USE_MESSAGE_DUMP = True
+    SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
+    LOAD = []
+    NO_LOAD = ['translation']
